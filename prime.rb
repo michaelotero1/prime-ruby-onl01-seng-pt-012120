8f6b006e8471b16c1b2400a71ca7e38a75781460
michael otero <michaelotero1118@gmail.com>
2020-02-02 22:04:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -22,10 +22,10 @@ def sieve(max)
     # Break if we are past the square root of the max value 
     break if p*p > max
     counter += 1
-    # Start at the square of the current number, and step through.
-    # Go up to the max value, by multiples of the current number, and replace
+  replace
     # that value with nil in the primes array
-    (p*p).step(max,p) { |m| primes[m] = nil }
+    (p*p).step(max,p) { |m| primes[m] = nil }  # Start at the square of the current number, and step through.
+    # Go up to the max value, by multiples of the current number, and 
   end
 
   # Finally, return the compacted array.
